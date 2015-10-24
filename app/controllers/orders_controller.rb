38d10299141061945ab4041c6387492a883b315a
Hao Li <hao.li@vodafone.com.au>
2015-10-24 15:14:10 +0800

    add a uuid gem and order action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,8 +3,26 @@ class OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find_by_oid(params[:id])
+    render :json => @order
   end
 
   def create
+    order_details = params[:order_details]
+
+    return if order_details.to_a.empty?
+
+    @order = Order.new
+
+    order_details.each do |detail|
+      @order.order_details << OrderDetail.create(detail)
+    end
+
+    @order.user = User.first
+    @order.oid= UUID.random_create.to_s
+    @order.save
+
+    render :json => { order: @order }
+
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
   def show
+    @product = Product.find_by_pid(params[:id])
+    render :json => @product
   end
+
 end
