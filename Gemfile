a0c0f460e3076c497ed7a3176b16d5aa59ae8207
Yanqing Wang <2441570482@qq.com>
2015-10-24 17:24:44 +0800

    enabled upload

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'uuidtools'
 gem 'twitter-bootstrap-rails'
 
 gem 'pg'
+gem 'carrierwave'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -41,7 +42,6 @@ gem 'pg'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = User.create({uid: "20151024001", name: "test01", pass: "123456"})
 product = Product.create({pid: "6920202888883", name: "RedBull", price: 6.00, desc: "This is a Red Bull"})
-product = Product.create({pid: "6920202888883", name: "RedBull", price: 6.00, desc: "This is a Red Bull"})
-product = Product.create({pid: "6920202888883", name: "RedBull", price: 6.00, desc: "This is a Red Bull"})
-product = Product.create({pid: "6920202888883", name: "RedBull", price: 6.00, desc: "This is a Red Bull"})
-product = Product.create({pid: "6920202888883", name: "RedBull", price: 6.00, desc: "This is a Red Bull"})
 order = Order.create({oid: "2015102400001", user: user, status: false, amount: 12.00})
 order_detail = OrderDetail.create({order: order, product: product, count: 2, price: 6.00})
+
+File.open('public/hongniu.jpg') do |f| product.avatar = f end
+product.save	
