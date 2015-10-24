e63b6b2fcb5a7a8cb90de4f2343dd23e7a0828db
Yanqing Wang <2441570482@qq.com>
2015-10-24 16:21:40 +0800

    add delete function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  
+  before_action :find_a_product, only: [:destroy]
+
   def show
     @product = Product.find_by_pid(params[:id])
     render :json => @product
@@ -6,6 +9,16 @@ class ProductsController < ApplicationController
 
   def index
   	@products = Product.all
+  end
+
+  def destroy
+  	@product.destroy
+    redirect_to products_path
+  end
+
+  private 
+  def find_a_product
+    @product = Product.find(params[:id])
   end
 
 end
