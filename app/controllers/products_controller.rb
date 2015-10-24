62cd8924063a0232ed9fd82f3129a1e584cf16a4
Yanqing Wang <2441570482@qq.com>
2015-10-24 18:51:34 +0800

    add new function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,20 @@
 class ProductsController < ApplicationController
   
   before_action :find_a_product, only: [:destroy]
+  
+  def new
+  	@product = Product.new
+  end
+
+  def create
+  	product = Product.new(product_param)
+  	if product.save
+  		product.save
+  		redirect_to products_path
+  	else
+  		render :new
+  	end
+  end
 
   def show
     @product = Product.find_by_pid(params[:id])
@@ -24,6 +38,10 @@ class ProductsController < ApplicationController
   private 
   def find_a_product
     @product = Product.find(params[:id])
+  end
+
+  def product_param
+  	params.require(:product).permit(:pid, :name, :price, :avatar, :desc)
   end
 
 end
